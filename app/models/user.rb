3a33d8363e85270b00ca1dfa0150bdb11ddd070f
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-09-16 10:51:41 -0500

    Fix rubocop issues

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
   has_many :reservations
   has_many :vehicles, through: :reservations
-  
+
   devise :database_authenticatable, :registerable, :jwt_authenticatable,
          jwt_revocation_strategy: JwtDenylist
-  
+
   validates :username, presence: true, length: { minimum: 6, maximum: 128 }
   validates :role, inclusion: { in: %w[user admin],
                                 message: '%<value>s is not a valid role' }
