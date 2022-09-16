43df7453c92cbcfd76449618c40d7ddc19c928ab
emiliazm <98142626+emiliazm@users.noreply.github.com>
2022-09-17 00:13:18 +0200

    Fix vehicle index controller.

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -2,15 +2,14 @@ class VehiclesController < ApplicationController
   load_and_authorize_resource
 
   def index
-    if can? :manage, Vehicle
+    if can? :manage, @vehicle
       render json: Vehicle.vehicles
     else
       render json: Vehicle.vehicles.where(visible: true)
     end
   end
 
-
   def vehicle_params
-    params.require(:vehicle).permit(:name, :price, :image, :visible)
+    params.require(:vehicle).permit(:name, :price, :image, :visible).merge(user_id: current_user.id)
   end
 end
