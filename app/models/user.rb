95196cf633f9531a80998eb3155bb8f5d39c14ae
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-09-16 10:51:01 -0500

    Add all validations, fix conflicts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ApplicationRecord
+  has_many :reservations
+  has_many :vehicles, through: :reservations
+  
+  devise :database_authenticatable, :registerable, :jwt_authenticatable,
+         jwt_revocation_strategy: JwtDenylist
+  
+  validates :username, presence: true, length: { minimum: 6, maximum: 128 }
+  validates :role, inclusion: { in: %w[user admin],
+                                message: '%<value>s is not a valid role' }
+
+  def all_reservations
+    reservations
+  end
 end
