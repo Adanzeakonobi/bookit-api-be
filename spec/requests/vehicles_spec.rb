981e3fff90979c277c5e454d089b9f83ab291f89
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-09-20 13:38:58 -0500

    Set up rswag with vehicles specs

---
+++ spec/requests/vehicles_spec.rb
@@ -0,0 +1,86 @@
+require 'swagger_helper'
+
+RSpec.describe 'vehicles', type: :request do
+
+  path '/vehicles' do
+
+    get('list vehicles') do
+      tags 'Vehicles'
+      response(200, 'successful') do
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+
+    post('create vehicle') do
+      tags 'Vehicles'
+      consumes 'application/json'
+      parameter name: :vehicle, in: :body, schema: {
+        type: :object,
+        properties: {
+          name: { type: :string },
+          price: { type: :number, format: :float },
+          image: { type: :string },
+          visible: { type: :boolean }
+        },
+        required: ['name', 'price', 'image', 'visible']
+      }
+
+      response(200, 'successful') do
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+
+  path '/vehicles/{id}' do
+    # You'll want to customize the parameter types...
+    parameter name: 'id', in: :path, type: :string, description: 'id'
+
+    get('show vehicle') do
+      tags 'Vehicles'
+      response(200, 'successful') do
+        let(:id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+
+    patch('update vehicle') do
+      tags 'Vehicles'
+      response(200, 'successful') do
+        let(:id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+end
