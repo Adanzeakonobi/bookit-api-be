2832fa519baf88d9396bf6d481f559a9752c3762
emiliazm <98142626+emiliazm@users.noreply.github.com>
2022-09-21 23:28:50 +0200

    Replace port.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -56,7 +56,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
 
   # Devise
-  config.action_mailer.default_url_options = { host: 'localhost', port: 4000 }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 5000 }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -45,7 +45,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   # Devise
-  config.action_mailer.default_url_options = { host: 'localhost', port: 4000 }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 5000 }
 
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
