36d32b58a37ef8e0e6b0b2b25fdd6b5cab29ade8
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-09-28 12:21:34 -0500

    Add user model specs

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  context 'validations' do
+    before do
+      @user = create(:user)
+    end
+
+    describe 'pass' do
+      it 'when attributes are present' do
+        expect(@user).to be_valid
+        expect(@user.username.length).to be >= 6
+        expect(@user.username.length).to be <= 128
+      end
+    end
+
+    describe 'nil username' do
+      it 'is not valid' do
+        @user.username = nil
+        expect(@user).to_not be_valid
+      end
+    end
+
+    describe 'nil role' do
+      it 'is not valid' do
+        @user.role = nil
+        expect(@user).to_not be_valid
+      end
+    end
+  end
+end
